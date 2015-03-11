9c1b3eeeb07ec0b2c2a8d2aaf138b44298b903be
Nav Appaiya <navarajh@gmail.com>
2015-03-11 23:11:41 +0100

    added twitter as a service container

---
+++ src/Nav/CMSBundle/Controller/TestController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Nav\CMSBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class TestController extends Controller
+{
+    public function indexAction()
+    {
+        
+    }
+
+}
