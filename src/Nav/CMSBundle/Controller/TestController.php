ea3ec94119a6da7efc53d6cbe20319b859bde2f5
Nav Appaiya <navarajh@gmail.com>
2015-03-11 23:43:05 +0100

    configured twitter oauth as a service

--- src/Nav/CMSBundle/Controller/TestController.php
+++ src/Nav/CMSBundle/Controller/TestController.php
@@ -2,6 +2,7 @@
 
 namespace Nav\CMSBundle\Controller;
 
+use GuzzleHttp\Client;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -10,7 +11,17 @@ class TestController extends Controller
 {
     public function indexAction()
     {
-        
+
+        $twitterClient = $this->get('guzzle.twitter.client');
+
+        $request = $twitterClient->post('statuses/update.json', null, array(
+            'status'=> 'Working on a new Feed to aggregate from, maintenance mode!'
+        ));
+
+
+        $data = $request->send()->json();
+        var_dump($data);exit;
+
     }
 
 }
