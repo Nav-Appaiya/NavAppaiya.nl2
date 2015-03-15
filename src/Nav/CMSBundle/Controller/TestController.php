3d860268da195bd075f88c63c177b901ba9e8f5a
Nav Appaiya <navarajh@gmail.com>
2015-03-15 13:44:34 +0100

    added a testing view under Default

--- src/Nav/CMSBundle/Controller/TestController.php
+++ src/Nav/CMSBundle/Controller/TestController.php
@@ -11,14 +11,10 @@ class TestController extends Controller
 {
     public function indexAction()
     {
-        $notifications = $this->get('nav.notification');
-
-        $notifications->add("test1", array("type" => "instant", "message" => "This is awesome"));
-        $notifications->add("test2", array("type" => "instant", "message" => "This is awesome"));
-
-        return $this->render('NavNotificationBundle:Notifications:success.html.twig', [
-            'notifications'=>$notifications
-        ]);
+        $bulk = ['something'=> [
+            'key'=>'value'
+        ]];
+        return $this->renderSomething($bulk);
 
     }
 
@@ -27,6 +23,23 @@ class TestController extends Controller
         $client = new Client();
         $response = $client->get("http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&q=" . $feedUrl)->json(['object'=>true]);
         return $response->responseData->feed;
+    }
+
+    public function notification()
+    {
+        $notifications = $this->get('nav.notification');
+
+        $notifications->add("test1", array("type" => "instant", "message" => "This is awesome"));
+        $notifications->add("test2", array("type" => "instant", "message" => "This is awesome"));
+
+        return $this->render('NavNotificationBundle:Notifications:success.html.twig', [
+            'notifications'=>$notifications
+        ]);
+    }
+
+    public function renderSomething($values)
+    {
+        return $this->render('@NavCMS/Default/test.html.twig', $values);
     }
 
 }
