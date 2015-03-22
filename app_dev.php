2b25f31c0cf7b92ab110974af03522295987677b
Nav Appaiya <navarajh@gmail.com>
2015-03-22 21:16:39 +0100

    development/production fix

--- app_dev.php
+++ app_dev.php
@@ -9,6 +9,16 @@ use Symfony\Component\HttpFoundation\Request;
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
+if (isset($_SERVER['HTTP_CLIENT_IP'])
+    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+    || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1', '81.205.39.253'))
+) {
+    header('HTTP/1.0 403 Forbidden');
+    exit('You are not allowed to access this file. Check ' . basename(__FILE__) . ' for more information.');
+}
+
+// This check prevents access to debug front controllers that are deployed by accident to production servers.
+// Feel free to remove this, extend it, or make something more sophisticated.
 
 $loader = require_once __DIR__ . '/app/bootstrap.php.cache';
 Debug::enable();
