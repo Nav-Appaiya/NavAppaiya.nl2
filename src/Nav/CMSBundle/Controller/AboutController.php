9fc5e6617c93b39c672fc17b62471d05b4730365
Nav Appaiya <navarajh@gmail.com>
2015-04-06 22:36:36 +0200

    added badgebackpack reference from mozilla

--- src/Nav/CMSBundle/Controller/AboutController.php
+++ src/Nav/CMSBundle/Controller/AboutController.php
@@ -9,13 +9,21 @@
 namespace Nav\CMSBundle\Controller;
 
 
+use GuzzleHttp\Client;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class AboutController extends Controller{
 
     public function indexAction() {
+        $codeschool = "http://backpack.openbadges.org/displayer/160839/group/53555.json";
 
-        return $this->render('NavCMSBundle:Default:about.html.twig');
+        $response = json_decode(file_get_contents($codeschool));
+        $badges = $response->badges;
+
+
+        return $this->render('NavCMSBundle:Default:about.html.twig', [
+            'badges' => $badges
+        ]);
     }
 
 }
